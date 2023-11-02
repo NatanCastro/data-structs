e144b0bb81f461be162b84e7f43581714d66c86a
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-11-01 23:39:10 -0300

    add append and create node functions to binary tree

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -3,6 +3,37 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void append_node(btn_t *head, btn_t *node) {
+  if (head == NULL) {
+    head = node;
+    return;
+  }
+  if (head->value > node->value) {
+    if (head->left == NULL) {
+      head->left = node;
+    } else {
+      append_node(head->left, node);
+    }
+  } else {
+    if (head->right == NULL) {
+      head->right = node;
+    } else {
+      append_node(head->right, node);
+    }
+  }
+}
+
+btn_t *create_node(int value) {
+  btn_t *node = (btn_t *)malloc(sizeof(btn_t));
+
+  node->value = value;
+  node->left = NULL;
+  node->right = NULL;
+
+  return node;
+}
 
 void print_pre_order(btn_t *head) {
   if (head == NULL)
@@ -71,36 +102,25 @@ void print_current_level(btn_t *node, uint32_t level) {
 }
 
 void binary_tree_example(void) {
-  btn_t head = {
-      .value = 4,
-  };
+  btn_t *head = create_node(4);
+  btn_t *node1 = create_node(3);
+  btn_t *node2 = create_node(5);
+  btn_t *node3 = create_node(7);
+  btn_t *node4 = create_node(2);
+  btn_t *node5 = create_node(1);
 
-  btn_t headLeft = {
-      .value = 10,
-  };
-  btn_t headRight = {
-      .value = 15,
-  };
-
-  head.left = &headLeft;
-  head.right = &headRight;
-
-  btn_t leftLeft = {
-      .value = 9,
-  };
-  btn_t leftRight = {
-      .value = 18,
-  };
-
-  headLeft.left = &leftLeft;
-  headLeft.right = &leftRight;
+  append_node(head, node1);
+  append_node(head, node2);
+  append_node(head, node3);
+  append_node(head, node4);
+  append_node(head, node5);
 
   printf("pre-order: ");
-  print_pre_order(&head);
+  print_pre_order(head);
   printf("\nin-order: ");
-  print_in_order(&head);
+  print_in_order(head);
   printf("\npost-order: ");
-  print_post_order(&head);
+  print_post_order(head);
   // printf("\nper-level: ");
   // print_per_level(&head);
   printf("\n");
--- src/binary_tree.h
+++ src/binary_tree.h
@@ -12,11 +12,13 @@ typedef struct BinaryTreeNode {
 } btn_t;
 
 uint32_t height(btn_t *);
+void append_node(btn_t *, btn_t *);
+btn_t *create_node(int);
 void print_pre_order(btn_t *);
 void print_in_order(btn_t *);
 void print_post_order(btn_t *);
 void print_per_level(btn_t *);
-void print_current_level(btn_t *node, uint32_t height);
+void print_current_level(btn_t *, uint32_t);
 void binary_tree_example(void);
 
-#endif
+#endif // BINARY_TREE_H_
